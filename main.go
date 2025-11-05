7656117dcb94cee6c37d23391d665ef7788248cd
Hauke <hauke.konrad@gmail.com>
2025-11-05 08:20:36 +0100

    Add main function and greeting function

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hallo von GitHub Actions!")
+}
+
+// Eine einfache Funktion zum Testen
+func Begruessung(name string) string {
+    return fmt.Sprintf("Hallo, %s!", name)
+}
